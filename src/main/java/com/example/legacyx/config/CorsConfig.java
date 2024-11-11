1ed4b984d5e01ea5d4479a439bdba0f0db8fb915
hliosone <sstelcher@protonmail.com>
2024-11-11 21:29:18 +0100

    Update test Cors

--- src/main/java/com/example/legacyx/config/CorsConfig.java
+++ src/main/java/com/example/legacyx/config/CorsConfig.java
@@ -18,7 +18,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins(allowedOrigin)
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
